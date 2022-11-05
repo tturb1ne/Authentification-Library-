03715b3f5861bdbd912c10d6ba4bfd710654f8eb
tturb1ne <expiloarmys@gmail.com>
2022-11-05 22:01:16 +0300

    first commit

---
+++ UserManagmentLibrary/Calculations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagmentLibrary
+{
+    public class Calculations
+    {
+        public string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consulationTime)
+        {
+            return new string[1];
+        }
+    }
+}
